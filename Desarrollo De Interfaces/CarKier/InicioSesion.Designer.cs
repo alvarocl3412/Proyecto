3ae3d2a9f6273b81b68e0abde7513c352eb46781
alvarocl3412 <alvarito341212@gmail.com>
2024-07-15 16:16:24 +0200

    Inicio Sesion Interfaz

---
+++ Desarrollo De Interfaces/CarKier/InicioSesion.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace CarKier
+{
+    partial class InicioSesion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InicioSesion));
+            this.btnInicioSesion = new System.Windows.Forms.Button();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.txtCorreoElectronico = new System.Windows.Forms.TextBox();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.pbOjo = new System.Windows.Forms.PictureBox();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOjo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInicioSesion
+            // 
+            this.btnInicioSesion.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnInicioSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicioSesion.Location = new System.Drawing.Point(312, 335);
+            this.btnInicioSesion.Margin = new System.Windows.Forms.Padding(13);
+            this.btnInicioSesion.Name = "btnInicioSesion";
+            this.btnInicioSesion.Size = new System.Drawing.Size(195, 40);
+            this.btnInicioSesion.TabIndex = 0;
+            this.btnInicioSesion.Text = "Iniciar Sesion";
+            this.btnInicioSesion.UseVisualStyleBackColor = true;
+            this.btnInicioSesion.Click += new System.EventHandler(this.btnInicioSesion_Click);
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Location = new System.Drawing.Point(366, 227);
+            this.lblCorreo.Margin = new System.Windows.Forms.Padding(13);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(88, 13);
+            this.lblCorreo.TabIndex = 2;
+            this.lblCorreo.Text = "Correo Elctronico";
+            // 
+            // txtCorreoElectronico
+            // 
+            this.txtCorreoElectronico.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtCorreoElectronico.BackColor = System.Drawing.Color.Snow;
+            this.txtCorreoElectronico.Location = new System.Drawing.Point(312, 243);
+            this.txtCorreoElectronico.Margin = new System.Windows.Forms.Padding(13);
+            this.txtCorreoElectronico.Name = "txtCorreoElectronico";
+            this.txtCorreoElectronico.Size = new System.Drawing.Size(195, 20);
+            this.txtCorreoElectronico.TabIndex = 3;
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(384, 281);
+            this.lblContraseña.Margin = new System.Windows.Forms.Padding(13);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(61, 13);
+            this.lblContraseña.TabIndex = 4;
+            this.lblContraseña.Text = "Contraseña";
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtContrasena.Location = new System.Drawing.Point(312, 297);
+            this.txtContrasena.Margin = new System.Windows.Forms.Padding(13);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.PasswordChar = '*';
+            this.txtContrasena.Size = new System.Drawing.Size(195, 20);
+            this.txtContrasena.TabIndex = 5;
+            // 
+            // pbOjo
+            // 
+            this.pbOjo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pbOjo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbOjo.Image = global::CarKier.Properties.Resources.ojo1;
+            this.pbOjo.Location = new System.Drawing.Point(503, 297);
+            this.pbOjo.Margin = new System.Windows.Forms.Padding(13);
+            this.pbOjo.Name = "pbOjo";
+            this.pbOjo.Size = new System.Drawing.Size(26, 20);
+            this.pbOjo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOjo.TabIndex = 6;
+            this.pbOjo.TabStop = false;
+            this.pbOjo.Click += new System.EventHandler(this.pbOjo_Click);
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pbLogo.Image = global::CarKier.Properties.Resources.logo;
+            this.pbLogo.Location = new System.Drawing.Point(312, 51);
+            this.pbLogo.Margin = new System.Windows.Forms.Padding(13);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(195, 164);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLogo.TabIndex = 1;
+            this.pbLogo.TabStop = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(312, 396);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(13);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(195, 40);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // InicioSesion
+            // 
+            this.AcceptButton = this.btnInicioSesion;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.pbOjo);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.txtCorreoElectronico);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.pbLogo);
+            this.Controls.Add(this.btnInicioSesion);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "InicioSesion";
+            this.Text = "Inicio Sesion";
+            this.SizeChanged += new System.EventHandler(this.InicioSesion_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pbOjo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInicioSesion;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.TextBox txtCorreoElectronico;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.PictureBox pbOjo;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Desarrollo De Interfaces/CarKier/InicioSesion.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarKier
+{
+    public partial class InicioSesion : Form
+    {
+        private bool mostrarContraseña = false;
+        public InicioSesion()
+        {
+            InitializeComponent();
+            this.Resize += InicioSesion_SizeChanged; // Suscripción al evento Resize
+
+
+        }
+
+        //Para mostrar la contraseña
+
+        private void pbOjo_Click(object sender, EventArgs e)
+        {
+            // Alternar entre mostrar y ocultar la contraseña
+            mostrarContraseña = !mostrarContraseña;
+
+            if (mostrarContraseña) {
+                // Mostrar contraseña
+                txtContrasena.PasswordChar = '\0';
+            } else {
+                // Ocultar contraseña
+                txtContrasena.PasswordChar = '*';
+            }
+        }
+
+
+
+        //Boton de inicio sesio comprobar que no estan los campos en blancos, comprobar que existe el
+        //Usuario y comprobar que es administrador y si es iniciar sesion correctamente si no mensaje de error
+        private void btnInicioSesion_Click(object sender, EventArgs e)
+        {
+            if (validarDatos())
+            {
+               
+            }
+        }
+
+        public bool validarDatos()
+        {
+            if (string.IsNullOrEmpty(txtCorreoElectronico.Text))
+            {
+                MessageBox.Show("Debes introducir el correo electronico", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtCorreoElectronico.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtContrasena.Text))
+            {
+                MessageBox.Show("Debes introducir la contraseña", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtContrasena.Focus();
+                return false;
+            }
+
+            return true;
+
+        }
+
+        private void InicioSesion_SizeChanged(object sender, EventArgs e)
+        {
+            int espacioEntreControles = 0;
+            //Calcular los Labels
+            lblCorreo.Left = (ClientSize.Width - lblCorreo.Width) / 2;
+            lblContraseña.Left = (ClientSize.Width - lblContraseña.Width) / 2;
+
+
+            // Calcular Los TextBox y la imagen del ojo para la contraseña
+            txtCorreoElectronico.Left = (ClientSize.Width - txtCorreoElectronico.Width) / 2;
+            int nuevoLeftTxtContraseña = (ClientSize.Width - txtContrasena.Width) / 2;
+            txtContrasena.Left =nuevoLeftTxtContraseña;
+            pbOjo.Left = nuevoLeftTxtContraseña + txtContrasena.Width + espacioEntreControles;
+           
+            //Calcular los botones
+            btnInicioSesion.Left = (ClientSize.Width - btnInicioSesion.Width) / 2;
+            btnSalir.Left = (ClientSize.Width - btnSalir.Width) / 2;
+
+        }
+    }
+}
